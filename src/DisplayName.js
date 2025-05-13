97926787b6855a8c9972e58f2127e51d96ae3b8f
AnirudhKumar99 <gvanirudh1999@gmail.com>
2025-05-13 16:39:17 +0530

    Initial commit

---
+++ src/DisplayName.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+export const DisplayName = () => {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [fullname, setFullName] = useState("");
+
+  const handleSubmit = (e) => {
+    if(firstname === "" || lastname === "") {
+      return;
+    }
+    setFullName(`${firstname} ${lastname}`);
+    e.preventDefault();
+  }
+  return (
+    <div>
+        <h1>Full Name Display</h1>
+      <form  style={{marginTop: "20px",marginBottom: "20px"}} onSubmit={handleSubmit}>
+        <label htmlFor="firstname">FirstName:</label>
+        <input
+          type="text"
+          id="firstname"
+          name="firstname"
+          value={firstname}
+          width={"100px"}
+          onChange={(e) => setFirstname(e.target.value)}
+          required
+        />
+        <br />
+        <label htmlFor="lastname">Lastname:</label>
+        <input
+          type="text"
+          id="lastname"
+          name="lastname"
+          value={lastname}
+          onChange={(e) => setLastname(e.target.value)}
+          required
+        />
+        <br />
+        <button
+          type="submit">submit</button>
+      </form>
+      <p style={{display:fullname!==""?"block":"none"}}>Full Name: {firstname} {lastname} </p>
+    </div>
+  );
+};
